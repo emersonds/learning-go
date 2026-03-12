50fdc573a63650344de598414a622647e0981e4f
Dylan Emerson <emersonds2868@gmail.com>
2026-03-11 20:14:32 -0400

    Leetcode: Partial solution for Merge Sorted Array

--- leetcode/mergesortedarray/mergesortedarray.go
+++ leetcode/mergesortedarray/mergesortedarray.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-func merge(nums1 []int, m int, nums2 []int, n int) {
+func merge(nums1 []int, m int, nums2 []int, n int) []int {
 	for i := m; i < m+n; i++ {
+		// Compiler error if m = 0
 		nums1[i] = nums2[i-n]
 	}
-	fmt.Println(nums1)
+
+	sort.Ints(nums1)
+
+	return nums1
 }
 
 func main() {
-	testNums1 := []int{1, 2, 3, 0, 0, 0}
-	testNums2 := []int{2, 5, 6}
+	testNums1 := []int{0}
+	testNums2 := []int{1}
+	m := 0
+	n := 1
+
+	fmt.Println(merge(testNums1, m, testNums2, n))
 }
